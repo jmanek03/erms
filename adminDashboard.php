98960bbab4f28bcaae18c68aa2d5c3d4d4cab60e
Mihir Manek <mihir.manek@somaiya.edu>
2024-01-24 21:32:17 +0530

    Add an Admin Dashboard 24-1-24

---
+++ adminDashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-COMPATIBLE" content="IE-EDGE">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="signup.css">
+  <link rel="shortcut-icon" type="image/x-icon" href="favicon.ico">
+  <title>K. J. SOMAIYA INSTITUTE OF TECHNOLOGY</title>
+</head>
+
+<body>
+  <div class="header">
+    <img src="logo1.png">
+    <span id="college"> K. J. SOMAIYA INSTITUTE OF TECHNOLOGY</span>
+    <span id="address">K. J. Somaiya Institue Of Technology, Somaiya, Sion, Mumbai, Maharashtra 400022</span>
+  </div>
+  <hr>
+</body>
+</html>
