c9ee3c4b87526c4d9df9e3d1ae6a7f6a0f2e0ec9
Jay Manek <122289114+jmanek03@users.noreply.github.com>
2024-12-02 10:09:57 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPOtJjYz6tu0OPQlTGGiiH8xdpc60PwlY",
-  authDomain: "college-website-7b3eb.firebaseapp.com",
-  projectId: "college-website-7b3eb",
-  storageBucket: "college-website-7b3eb.appspot.com",
-  messagingSenderId: "903210337288",
-  appId: "1:903210337288:web:4c5f8ccd853e04e2fcfdbd",
-  measurementId: "G-F932L9YXM2"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
@@ -27,7 +27,7 @@ function doUserLogin(){
       const credential = GoogleAuthProvider.credentialFromResult(result)
       const token = credential.accessToken
       const user = result.user
-      if (user.email.endsWith("@somaiya.edu")) {
+      if (user.email.endsWith("@")) {
           localStorage.setItem('isAuth', true)
           setIsAuth(true)
           navigate('/userDashboard.php');
